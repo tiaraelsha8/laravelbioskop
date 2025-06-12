86a206180c480b6a7c3d394a310423b5bc395541
tiaraelsha8 <elshawinatiara@gmail.com>
2025-06-12 09:36:02 +0700

    edit route

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,10 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
 Route::middleware('guest')->group(function () {
     Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
     Route::post('/login', [AuthController::class, 'login'])->name('login.post');
@@ -21,16 +25,16 @@ Route::middleware('auth')->group(function () {
         return view('admin.bioskop');
     })->name('admin.dashboard');
 
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-});
-
-Route::get('/admin/film', function () {
+    Route::get('/admin/film', function () {
         return view('admin.daftarfilm');
     })->name('admin.film');
 
-Route::get('/admin/jadwal', function () {
+    Route::get('/admin/jadwal', function () {
         return view('admin.jadwaltayang');
     })->name('admin.jadwal');
+
+    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
+});
 
 // Route::get('/admin/user', function () {
 //         return view('admin.listuser');
